f1347a2acafc0d4723043bc2763b98d48e5aaa8b
Evan Peterson <petersonev@Evans-MacBook-Pro.local>
2015-03-18 19:09:02 -0400

    Initial Commit

---
+++ CAS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CAS
+//
+//  Created by Evan Peterson on 3/18/15.
+//  Copyright (c) 2015 Evan Peterson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
